607b818f782228421fa31ab3d11b2d042aaf0444
radkam2000 <radkam2000@protonmail.com>
2023-10-21 14:57:01 +0200

    done lab 6

--- lab6/src/main/java/org/example/Main.java
+++ lab6/src/main/java/org/example/Main.java
@@ -1,9 +1,52 @@
 package org.example;
 
-
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String scannerInput;
+        System.out.println("Podaj imie");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        String imie = scannerInput;
+        System.out.println("Podaj nazwisko");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        String nazwisko = scannerInput;
+        System.out.println("Podaj indeks");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        Integer indeks = Integer.valueOf(scannerInput);
+        System.out.println("Podaj maila");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        String mail = scannerInput;
+        System.out.println("Podaj adres");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        String address = scannerInput;
+        System.out.println("Podaj rok urodzenia");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        Integer rok = Integer.valueOf(scannerInput);
+        System.out.println("Podaj miesiąc urodzenia");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        Integer miesiac = Integer.valueOf(scannerInput);
+        System.out.println("Podaj dzień urodzenia");
+        scannerInput = scanner.nextLine();
+        checkInput(scannerInput);
+        Integer dzien = Integer.valueOf(scannerInput);
+        Student s = new Student(imie, nazwisko, indeks, mail, address, new Date(rok-1900, miesiac-1, dzien));
+        System.out.println(s.toString());
+    }
 
+    public static void checkInput(String input) {
+        if (input.length() < 1) {
+            System.out.println("Nie podałeś danych");
+            System.exit(1);
+        }
     }
 }
--- lab6/src/main/java/org/example/Student.java
+++ lab6/src/main/java/org/example/Student.java
@@ -1,55 +1,20 @@
 package org.example;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
-    public String name;
+    private @NonNull String name;
+    private @NonNull String surname;
+    private @NonNull Integer index;
+    private @NonNull String email;
+    private @NonNull String address;
+    private @NonNull Date birthDate;
 
-    public String surname;
-
-    public Integer index;
-
-    public String email;
-
-    public Student() {
-    }
-
-    public Student(String name, String surname, Integer index, String email) {
-        this.name = name;
-        this.surname = surname;
-        this.index = index;
-        this.email = email;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getIndex() {
-        return index;
-    }
-
-    public void setIndex(Integer index) {
-        this.index = index;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
-
